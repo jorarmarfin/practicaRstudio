020ea504ba25125113ac60854e39846ef788addd
Luis Mayta <luis.mayta@gmail.com>
2018-12-06 00:51:40 -0500

    conexion a base de datos

--- script1.R
+++ script1.R
@@ -18,4 +18,19 @@ summarise(grupo1,n())
 
 #tabla de doble entrada
 tbl1<-table(data$sexo,data$edad)
-tbl1
+class(tbl1)
+write.csv(tbl1,"export/tbl1.csv",sep = ";")
+
+#Concetar a Base de Datos
+install.packages("RMariaDB")
+#hay que instalar libmariadbclient-dev | libmariadb-client-lgpl-dev (Debian, Ubuntu)
+#rpm: mariadb-connector-c-devel | mariadb-devel | mysql-devel (Fedora, CentOS, RHEL)
+library(RMariaDB)
+#Loading required package: DBI
+con <- dbConnect(MySQL(), group="mydrupal")
+dbListTables(con)
+
+query <- dbGetQuery(con,statement="SELECT * FROM users");
+sql<-"SELECT * FROM users limit 1"
+query2<-dbGetQuery(con,sql)
+
